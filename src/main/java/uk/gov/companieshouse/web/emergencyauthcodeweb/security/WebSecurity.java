c2b0ea4ce857c3e2323f262ac895ff73bc4cb85c
John Griffiths <jgriffiths2@companieshouse.gov.uk>
2026-07-21 17:45:38 +0100

    SIV-1253 enable CSRF support

--- src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/security/WebSecurity.java
+++ src/main/java/uk/gov/companieshouse/web/emergencyauthcodeweb/security/WebSecurity.java
@@ -2,38 +2,37 @@ package uk.gov.companieshouse.web.emergencyauthcodeweb.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import uk.gov.companieshouse.auth.filter.HijackFilter;
 import uk.gov.companieshouse.auth.filter.UserAuthFilter;
-import uk.gov.companieshouse.session.handler.SessionHandler;
+
+import static uk.gov.companieshouse.csrf.config.ChsCsrfMitigationHttpSecurityBuilder.configureWebCsrfMitigations;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurity {
 
     @Bean
+    @Order(1)
     public SecurityFilterChain authCodeSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .securityMatcher("/auth-code-requests/start", "/auth-code-requests/accessibility-statement")
-            .csrf().disable()
-            .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-            .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class);
-            ;
-        return http.build();
+        return configureWebCsrfMitigations(
+                http.securityMatcher("/auth-code-requests/start", "/auth-code-requests/accessibility-statement")
+                        .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+        ).build();
     }
 
     @Bean
+    @Order(2)
     public SecurityFilterChain eacWebSecurityFilterChain(HttpSecurity http) throws Exception {
-        http
-            .securityMatcher("/auth-code-requests/company/**", "/auth-code-requests/requests/**")
-            .csrf().disable()
-            .addFilterBefore(new SessionHandler(), BasicAuthenticationFilter.class)
-            .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
-            .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class);
-        return http.build();
+        return configureWebCsrfMitigations(
+                http.securityMatcher("/auth-code-requests/company/**", "/auth-code-requests/requests/**")
+                        .addFilterBefore(new HijackFilter(), BasicAuthenticationFilter.class)
+                        .addFilterBefore(new UserAuthFilter(), BasicAuthenticationFilter.class)
+        ).build();
     }
 }
 
